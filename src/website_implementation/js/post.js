6eb971df74943b39a4f240121ce5c06c3227b74b
SereneYe <sereneye130@gmail.com>
2024-03-31 21:52:06 +1000

    Fetching the data from published API

---
+++ src/website_implementation/js/post.js
@@ -0,0 +1,22 @@
+// const postCommunityEventMethod = "POST";
+
+// var formData = new FormData();
+// formData.append("name", "Yoga in a tree 4");
+// formData.append("location", "Morningside Forest");
+// formData.append("organiser", "Morningside Community Wellness Collective");
+// formData.append("event_type", "Group Activity");
+// formData.append("description", "Doing yoga in a tree");
+// formData.append("date_time", "2023-10-21 17:30");
+// // formData.append("photo", fileInput.files[0], "icons8-software-80.png");
+// formData.append("website_code", "Pete123");
+
+// const requestOptions2 = {
+//   method: postCommunityEventMethod,
+//   body: formData,
+//   redirect: "follow",
+// };
+
+// fetch(baseURL, requestOptions2)
+//   .then((response) => response.text())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log("error", error));

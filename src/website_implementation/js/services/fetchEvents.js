7cf952e72c80b52d4b6318209a7180deae7a5286
SereneYe <sereneye130@gmail.com>
2024-04-02 21:59:34 +1000

    Load data from API and implement some basic events rendering with the data

---
+++ src/website_implementation/js/services/fetchEvents.js
@@ -0,0 +1,24 @@
+export function fetchEvents() {
+  const baseURL =
+    "https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/community_events/";
+
+  const my_website_code = "serene12345";
+
+  const queryParams = {
+    website_code: my_website_code,
+  };
+
+  const queryString = new URLSearchParams(queryParams).toString();
+  const urlWithParams = baseURL + "?" + queryString;
+
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  return fetch(urlWithParams, requestOptions)
+    .then((response) => response.json())
+    .then((jsonResponse) => {
+      return jsonResponse;
+    });
+}

72d0857166cf82b144d9e6bcce145df22bf64498
SereneYe <sereneye130@gmail.com>
2024-04-16 23:09:28 +1000

    Finishing implementing all responsive mediaqueries in map, regiatration page

--- src/website_implementation/js/eventMap.js
+++ src/website_implementation/js/eventMap.js
@@ -266,3 +266,12 @@ class App {
 }
 
 const app = new App();
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
